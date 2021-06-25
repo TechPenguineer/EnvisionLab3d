71399305bb67dc2ef83ad424d2d8eaca18cac93e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-06-25 15:49:07 -0400

    create main display

---
+++ view/display.py
@@ -0,0 +1,12 @@
+import pygame
+run = True
+while run:
+    background_colour = (56,56,56)
+    side_nav_colour = (92, 92, 92)
+    canvas = pygame.display.set_mode([500,600])
+    canvas.fill(background_colour)
+    pygame.display.flip()
+    side_nav = pygame.Surface([50,600])
+    side_nav.fill
+pygame.quit()
+

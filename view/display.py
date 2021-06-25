69e4c3ae4b7ac9cbffc23cf7fae088b867578951
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-06-25 15:52:09 -0400

    create_panel

--- view/display.py
+++ view/display.py
@@ -5,8 +5,9 @@ while run:
     side_nav_colour = (92, 92, 92)
     canvas = pygame.display.set_mode([500,600])
     canvas.fill(background_colour)
-    pygame.display.flip()
     side_nav = pygame.Surface([50,600])
-    side_nav.fill
+    side_nav.fill(side_nav_colour)
+    canvas.blit(side_nav, [1,1])
+    pygame.display.flip()
 pygame.quit()
 
